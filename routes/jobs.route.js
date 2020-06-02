f0bc2cef128f3ff2b838d0b57cab51b03f230b2a
fishydev <huvuhuvuhuvu@gmail.com>
2020-06-02 15:31:51 +0700

    get all job route

--- routes/jobs.route.js
+++ routes/jobs.route.js
@@ -6,7 +6,9 @@ const jobController = require('../controllers/jobs.controller');
 // POST new job
 router.post('/post', jobController.postJob)
 
-router.get('/:q', jobController.getJobList)
+router.get('/', jobController.findJobByTitle)
+
+router.get('/all', jobController.getJobList)
 
 // router.get('/:id', jobController.findJobById)
 
