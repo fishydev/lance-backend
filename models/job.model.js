f3f501639c7b6643aea121ce2c77666f9f5d25ab
fishydev <huvuhuvuhuvu@gmail.com>
2020-06-02 14:47:14 +0700

    changed job model

--- models/job.model.js
+++ models/job.model.js
@@ -2,18 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var jobSchema = new Schema({
-    name: {type: String, required: true, maxlength: 30},
-    employer: {type: String, required: true, maxlength: 320},
-    freelancer: {type: String},
-    startDate: {type: Date, required: true},
-    deadline: {type: Date},
-    fee: {type: Number, required: true},
-    completed: {type: Boolean, required: true},
-    payment: {
-        paymentDate: {type: Date},
-        amount: {type: Number}
-    },
+    title: {type: String, required: true, maxlength: 30},
+    desc: {type: String, required: true, maxlength: 512},
     category: {type: String, required: true},
+    freelancer: {type: String, required: true, maxlength: 320},
+    fee: {type: Number, required: true},
 });
 
 module.exports = mongoose.model('Job', jobSchema);
